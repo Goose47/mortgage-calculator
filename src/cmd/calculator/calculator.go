cd896ffc5b13bbc0398fa0aad580453e1acd8d45
Nikita <tyumin000@gmail.com>
2024-11-07 17:16:32 +0300

    feat: add application entrypoint

--- src/cmd/calculator/calculator.go
+++ src/cmd/calculator/calculator.go
@@ -1,8 +1,18 @@
 // application entrypoint
 package main
 
-import "log"
+import (
+	"log/slog"
+	apppkg "mortgage-calculator/src/internal/app"
+	"mortgage-calculator/src/internal/config"
+	"mortgage-calculator/src/internal/logger"
+)
 
 func main() {
-	log.Println("Hello, world!")
+	cfg := config.MustLoad()
+	log := logger.New(cfg.Env)
+	app := apppkg.New(log, cfg.Env, cfg.Port)
+
+	err := app.Server.Serve()
+	log.Error("application has stopped: %s", slog.Any("error", err))
 }
