3ccb7f16d8a88dd011c36edb6ebfe204e43e4ca7
Nikita <tyumin000@gmail.com>
2024-11-07 14:38:32 +0300

    feat: add config package and sample config file

---
+++ src/internal/config/config.go
@@ -0,0 +1,79 @@
+// Package config describes required configuration and provides methods for reading configuration from files.
+package config
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+)
+
+var errFileNotExists = errors.New("config file does not exist")
+var errBadConfigFile = errors.New("unable to read config file")
+
+// Config represents main app configuration.
+type Config struct {
+	Env   string `yaml:"env"`
+	Port  string `yaml:"port"`
+	Cache Cache  `yaml:"cache"`
+}
+
+// Cache represents cache configuration.
+type Cache struct {
+	TTL int `yaml:"ttl"`
+}
+
+// LoadPath loads configuration from specified path and returns config instance and error.
+func LoadPath(configPath string) (*Config, error) {
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		return nil, fmt.Errorf("%w: %s", errFileNotExists, configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		return nil, fmt.Errorf("%w: %s", errBadConfigFile, err.Error())
+	}
+
+	return &cfg, nil
+}
+
+// MustLoad fetches path, loads configuration and panics on any error.
+func MustLoad() *Config {
+	configPath := fetchConfigPath()
+
+	cfg, err := LoadPath(configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	return cfg
+}
+
+// MustLoadPath loads configuration form configPath and panics on any error.
+func MustLoadPath(configPath string) *Config {
+	cfg, err := LoadPath(configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	return cfg
+}
+
+// fetchConfigPath fetches config path from command line flag or environment variable.
+// Priority: flag > env > default.
+// Default value is empty string.
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
