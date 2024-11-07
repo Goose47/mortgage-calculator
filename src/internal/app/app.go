aa4b6a1e81ccff83e304c17a0704c7f70e21a881
Nikita <tyumin000@gmail.com>
2024-11-07 17:14:08 +0300

    feat: add app creation logic

---
+++ src/internal/app/app.go
@@ -0,0 +1,27 @@
+// Package app defines application model.
+package app
+
+import (
+	"log/slog"
+	serverapp "mortgage-calculator/src/internal/app/server"
+	"mortgage-calculator/src/internal/server"
+)
+
+// App represents application.
+type App struct {
+	Server *serverapp.Server
+}
+
+// New creates all dependencies for App and returns new App instance.
+func New(
+	log *slog.Logger,
+	env string,
+	port int,
+) *App {
+	router := server.NewRouter(env)
+	serverApp := serverapp.New(log, port, router)
+
+	return &App{
+		Server: serverApp,
+	}
+}
