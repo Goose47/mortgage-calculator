2a2466d300899dec2f4e6ec715a7661edec4a6a1
Nikita <tyumin000@gmail.com>
2024-11-09 13:23:23 +0300

    feat: add cache controller

--- src/internal/api/controllers/cacheController.go
+++ src/internal/api/controllers/cacheController.go
@@ -1,16 +1,52 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"log/slog"
+	"mortgage-calculator/src/internal/domain/dto"
+	"net/http"
+)
+
+// CacheProvider interacts with cache.
+type CacheProvider interface {
+	List(ctx context.Context) ([]*dto.CacheEntry, error)
+}
 
 // CacheController deals with cache endpoints.
-type CacheController struct{}
+type CacheController struct {
+	log   *slog.Logger
+	cache CacheProvider
+}
 
 // NewCacheController is a constructor for CacheController.
-func NewCacheController() *CacheController {
-	return &CacheController{}
+func NewCacheController(
+	log *slog.Logger,
+	cache CacheProvider,
+) *CacheController {
+	return &CacheController{
+		log:   log,
+		cache: cache,
+	}
 }
 
 // List lists all active cache entries.
 func (con *CacheController) List(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "hello"})
+	ctx := c.Request.Context()
+	entries, err := con.cache.List(ctx)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to retrieve cache entries",
+		})
+		return
+	}
+	if len(entries) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "empty cache",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, entries)
 }
