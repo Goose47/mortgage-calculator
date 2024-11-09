632b34d9c29d0adc8f2b7a0d20bddc8fce76beef
Nikita <tyumin000@gmail.com>
2024-11-09 11:56:33 +0300

    feat: run background cache entries clearing

--- src/cmd/calculator/calculator.go
+++ src/cmd/calculator/calculator.go
@@ -2,16 +2,28 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	apppkg "mortgage-calculator/src/internal/app"
 	"mortgage-calculator/src/internal/config"
 	"mortgage-calculator/src/internal/logger"
+	"time"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	log := logger.New(cfg.Env)
-	app := apppkg.New(log, cfg.Env, cfg.Port)
+	app := apppkg.New(log, cfg.Env, cfg.Port, int64(cfg.Cache.TTL))
+
+	//todo graceful stop
+	go func() {
+		tick := time.NewTicker(time.Duration(cfg.Cache.Clear) * time.Second)
+		ctx := context.Background()
+		for {
+			<-tick.C
+			app.Cache.Clear(ctx)
+		}
+	}()
 
 	err := app.Server.Serve()
 	log.Error("application has stopped: %s", slog.Any("error", err))
