7fdd26bea2aaed01fc9c9add2a8ed9f9cd98a591
Nikita <tyumin000@gmail.com>
2024-11-09 13:23:43 +0300

    feat: add entry model to cache pkg

--- src/internal/cache/cache.go
+++ src/internal/cache/cache.go
@@ -1,7 +1,14 @@
-// Package cache contains errors common to every cache provider.
+// Package cache contains errors and types common to every cache provider.
 package cache
 
 import "errors"
 
 // ErrKeyNotExists represents error when key is not present in cache.
 var ErrKeyNotExists = errors.New("key is not found")
+
+// Entry describes data stored in cache.
+type Entry struct {
+	Key string
+	Val []byte
+	ID  int64
+}
