863ba4c686b113ab99e580384475f948a126e3f0
Nikita <tyumin000@gmail.com>
2024-11-09 11:57:04 +0300

    feat: create cache and repo instances in app constructor

--- src/internal/app/app.go
+++ src/internal/app/app.go
@@ -2,16 +2,24 @@
 package app
 
 import (
+	"context"
 	"log/slog"
 	"mortgage-calculator/src/internal/api/controllers"
 	serverapp "mortgage-calculator/src/internal/app/server"
+	"mortgage-calculator/src/internal/cache/memory"
+	cache_repos "mortgage-calculator/src/internal/cache/repos"
 	"mortgage-calculator/src/internal/server"
 	"mortgage-calculator/src/internal/services"
 )
 
+type Clearer interface {
+	Clear(ctx context.Context)
+}
+
 // App represents application.
 type App struct {
 	Server *serverapp.Server
+	Cache  Clearer
 }
 
 // New creates all dependencies for App and returns new App instance.
@@ -19,10 +27,14 @@ func New(
 	log *slog.Logger,
 	env string,
 	port int,
+	cacheTTL int64,
 ) *App {
+	cache := memory.New(log, cacheTTL)
+	repo := cache_repos.NewCalcRepository(log, cache)
+
 	calcService := services.NewCalculatorService(log)
 
-	calcCon := controllers.NewCalcController(calcService)
+	calcCon := controllers.NewCalcController(calcService, repo)
 	cacheCon := controllers.NewCacheController()
 
 	router := server.NewRouter(log, env, calcCon, cacheCon)
@@ -30,5 +42,6 @@ func New(
 
 	return &App{
 		Server: serverApp,
+		Cache:  repo,
 	}
 }
