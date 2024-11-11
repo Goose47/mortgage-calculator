0e22b00715f8d7171a3ebc2d66e759e6f33592fb
Nikita <tyumin000@gmail.com>
2024-11-11 12:00:45 +0300

    refactor: change payment date type

--- src/internal/domain/dto/calcAggregates.go
+++ src/internal/domain/dto/calcAggregates.go
@@ -1,13 +1,11 @@
 // Package dto provides dtos used across whole domain.
 package dto
 
-import "time"
-
 // CalcAggregates represents calculation result.
 type CalcAggregates struct {
-	LastPaymentDate time.Time `json:"last_payment_date"`
-	Rate            int       `json:"rate"`
-	LoanSum         int       `json:"loan_sum"`
-	MonthlyPayment  int       `json:"monthly_payment"`
-	Overpayment     int       `json:"overpayment"`
+	LastPaymentDate string `json:"last_payment_date"`
+	Rate            int    `json:"rate"`
+	LoanSum         int    `json:"loan_sum"`
+	MonthlyPayment  int    `json:"monthly_payment"`
+	Overpayment     int    `json:"overpayment"`
 }
