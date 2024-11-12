d3980c11c046fde441978a96292bfb099fe164bc
Nikita <tyumin000@gmail.com>
2024-11-12 15:38:25 +0300

    refactor: change to cryptographically secure generator

--- src/internal/lib/random/random.go
+++ src/internal/lib/random/random.go
@@ -1,16 +1,23 @@
+// Package random provides functions to generate cryptographically secure random entities.
 package random
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"fmt"
+	"math/big"
 )
 
-func String(n int) string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-	var byteStr []byte
-	for range n {
-		byteStr = append(byteStr, charset[rand.Intn(len(charset))])
+// String generates a cryptographically secure random string of length n.
+func String(n int) (string, error) {
+	byteStr := make([]byte, n)
+	for i := 0; i < n; i++ {
+		index, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return "", fmt.Errorf("failed to generate random string: %w", err)
+		}
+		byteStr[i] = charset[index.Int64()]
 	}
-
-	return string(byteStr)
+	return string(byteStr), nil
 }
